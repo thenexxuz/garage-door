0f93007da0e2bb1121713c8054ae048a8916a978
William Penton <william@penton.us>
2022-09-22 15:50:07 -0500

    remove gpio packages added test with gpio util

--- app/Console/Commands/TestSensor.php
+++ app/Console/Commands/TestSensor.php
@@ -3,8 +3,6 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use PiPHP\GPIO\GPIO;
-use PiPHP\GPIO\Pin\InputPinInterface;
 
 class TestSensor extends Command
 {
@@ -29,27 +27,11 @@ class TestSensor extends Command
      */
     public function handle()
     {
-        // Create a GPIO object
-        $gpio = new GPIO();
-
-        // Retrieve pin 18 and configure it as an input pin
-        $pin = $gpio->getInputPin(18);
-
-        // Configure interrupts for both rising and falling edges
-        $pin->setEdge(InputPinInterface::EDGE_BOTH);
-
-        // Create an interrupt watcher
-        $interruptWatcher = $gpio->createWatcher();
-
-        // Register a callback to be triggered on pin interrupts
-        $interruptWatcher->register($pin, function (InputPinInterface $pin, $value) {
-            echo 'Pin ' . $pin->getNumber() . ' changed to: ' . $value . PHP_EOL;
-
-            // Returning false will make the watcher return false immediately
-            return true;
-        });
-
-        // Watch for interrupts, timeout after 5000ms (5 seconds)
-        while ($interruptWatcher->watch(5000));
+        do {
+            unset($result);
+            exec("sudo raspi-gpio get 27 | awk '{print $3}'", $result);
+	    echo $result[0] . PHP_EOL;
+	} while($result[0] === 'level=1');
     }
 }
+
